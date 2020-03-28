c1acd3bf7fe4487e880c54e6389bf2bdb0ac9e9d
lizhenzhen <lizhenzhen@jimilab.com>
2020-03-28 19:04:35 +0800

    Router 路由配置

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -4,14 +4,32 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 export default new Router({
-    routes: [{
-        path: '/',
-        name: 'landing-page',
-        component: require('@/components/LandingPage').default
-    },
-    {
-        path: '*',
-        redirect: '/'
-    }
+    routes: [
+        {
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/',
+            name: 'landing-page',
+            component: require('@/components/LandingPage').default
+        },
+        {
+            path: '*',
+            redirect: '/'
+        },
+        {
+            path: '/main',
+            name: 'main',
+            redirect: '/home',
+            component: () => import('@/views/main.vue'),
+            children: [
+                {
+                    path: '/home',
+                    name: 'home',
+                    component: () => import('@/views/home.vue')
+                }
+            ]
+        }
     ]
 });
