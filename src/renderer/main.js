c17047064e303d4bac641334257af42492b61e1d
lizhenzhen <lizhenzhen@jimilab.com>
2020-03-28 19:06:02 +0800

    Config 主函数配置 引入element-ui 阿里图标 全局样式

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,9 +5,24 @@ import App from './App';
 import router from './router';
 import store from './store';
 
+import './styles/base.css';
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+/* 全局修改element尺寸为 medium | small */
+Vue.prototype.$ELEMENT = {
+    size: 'mini'
+};
+
+import './icons/iconfont.css'; // 字体图标 css
+import './icons/iconfont.js'; // 字体图标 js
 
 /* eslint-disable no-new */
 new Vue({
